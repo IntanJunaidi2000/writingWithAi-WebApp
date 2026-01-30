bf6421de0bf4a3f18b16c32c3a790d02765b8e50
IntanJunaidi2000 <intanmai2000@gmail.com>
2026-01-30 11:43:22 +0800

    Initial commit

---
+++ src/main/java/com/intanmaijunaidi/vis_writingwebapp/VisWritingwebappApplication.java
@@ -0,0 +1,13 @@
+package com.intanmaijunaidi.vis_writingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisWritingwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisWritingwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intanmaijunaidi/vis_writingwebapp/VisWritingwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intanmaijunaidi.vis_writingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisWritingwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
